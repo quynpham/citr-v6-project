53523ef3df72b0b79f0ac04f2e7049d293aab256
Quynh Anh Pham <qap.pham@gmail.com>
2022-03-23 13:26:15 +0100

    Server side rendering part 2

--- 12-portals-and-refs/src/App.js
+++ 12-portals-and-refs/src/App.js
@@ -1,18 +1,14 @@
-/* eslint-disable react/no-unescaped-entities */
-/* eslint-disable react/jsx-no-comment-textnodes */
-import { useState, StrictMode, lazy, Suspense } from "react";
+import { useState, StrictMode } from "react";
 import { Route, Switch, Link } from "react-router-dom";
+import Details from "./Details";
+import SearchParams from "./SearchParams";
 import ThemeContext from "./ThemeContext";
-
-const Details = lazy(() => import("./Details"));
-const SearchParams = lazy(() => import("./SearchParams"));
 
 const App = () => {
   const theme = useState("darkblue");
   return (
     <StrictMode>
       <ThemeContext.Provider value={theme}>
-        // eslint-disable-next-line react/jsx-no-comment-textnodes
         <div>
           <header>
             <Link to="/">Adopt Me!</Link>
